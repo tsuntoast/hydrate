2e917cd7517e76d677ed0a23db3927ad7604e119
tsuntoast <47610779+tsuntoast@users.noreply.github.com>
2021-04-30 18:50:14 -0700

    git commit ("Add text, input, and state"

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -3,7 +3,9 @@ import { SafeAreaView, View, Text, StyleSheet, Button, TextInput, Keyboard, Keyb
 
 const HomeScreen = props => {
 
-    const [searchTitle, setSearchTitle] = useState('');
+    const [amount, setAmount] = useState(0);
+    const [loggedAmount, setLoggedAmount] = useState(0);
+    const [units, setUnits] = useState('oz');
 
     return (
         <KeyboardAvoidingView
@@ -13,9 +15,30 @@ const HomeScreen = props => {
             <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
                 <SafeAreaView>
 
-                    <View>
-                        <Text>hi this is home</Text>
-                        
+                    <View style={styles.dailySummaryContainer}>
+                        <Text>You have drank</Text>
+                        <Text style={{ fontSize: 60 }}>{amount}</Text>
+                        <Text>{units} of water today.</Text>
+                    </View>
+
+                    <View style={{ paddingVertical: 20 , alignItems: 'center'}}>
+                        <TextInput style={styles.textInput} keyboardType='numeric' placeholder='Amount' value={loggedAmount} onChangeText={(text) => setLoggedAmount(text)} />
+
+                        <Button title="Submit" onPress={() => {
+
+                            if (!loggedAmount) { // If field is empty
+                                alert("Empty field. Please try again.");
+                            }
+                            else {
+                                setAmount(amount + Number(loggedAmount));
+
+                                // Resets text input field
+                                setLoggedAmount('');
+
+
+                            }
+
+                        }} />
                     </View>
                 </SafeAreaView>
             </TouchableWithoutFeedback>
@@ -30,6 +53,16 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    dailySummaryContainer: {
+        alignItems: 'center',
+    },
+    textInput: {
+        borderBottomWidth: 1,
+        borderColor: '#AA8A81',
+        backgroundColor: 'lightgrey',
+        padding: 4,
+        width: 100
     },
 
 });
