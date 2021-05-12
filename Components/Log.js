646c234d97bfe423e8cb2deb93f772594eab45b5
tsuntoast <47610779+tsuntoast@users.noreply.github.com>
2021-05-11 21:32:40 -0700

    Update

--- Components/Log.js
+++ Components/Log.js
@@ -1,18 +1,49 @@
 import React from 'react';
-import {} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import PropTypes from 'prop-types';
 
 const Log = props => {
 	return (
-		<TextInput style={styles.textInput} value={props.value} onChangeText={(text) => props.onChangeText(text)}/>
+		<View style={styles.rootContainer}>
+
+				<View style={styles.logContainer}>
+
+					<View style={styles.timeContainer}>
+						<Text style={styles.logText}>{props.time}</Text>
+					</View>
+
+					<View style={styles.intakeContainer}>
+						<Text style={styles.logText}>
+							{props.intake} {props.unit}
+						</Text>
+					</View>
+
+				</View>
+
+			</View>
 	);
 };
 
-const styles = Stylesheet.create({
-	textInput: {
-		backgroundColor: 'green',
-		width: '50%',
-		padding: 20
-	}
+Log.propTypes = { time: PropTypes.string.isRequired, intake: PropTypes.number.isRequired, unit: PropTypes.string.isRequired };
+
+const styles = StyleSheet.create({
+	rootContainer: {
+		flex: 1,
+		backgroundColor: 'lightgrey',
+		padding: 5,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	logContainer: {
+		flexDirection: 'row',
+	},
+	timeContainer: {
+		marginRight: 20,
+	},
+	logText: {
+		fontSize: 20,
+		fontWeight: 'bold',
+	},
 });
 
 export default Log;
